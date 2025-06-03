9d3df11e569785198dea5148c564a811f0fb9cd6
maricel <maricelmb@gmail.com>
2025-06-03 18:07:12 +1000

    S2.16: Add Model and dto of Coupon

---
+++ Restaurant.Services.CouponAPI/Model/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurant.Services.CouponAPI.Model
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public string DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
