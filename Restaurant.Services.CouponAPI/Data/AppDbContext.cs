673e9b0b6307ee8b7e8349242ef296df7b1ed330
maricel <maricelmb@gmail.com>
2025-06-05 12:50:11 +1000

    Seed data for coupon

--- Restaurant.Services.CouponAPI/Data/AppDbContext.cs
+++ Restaurant.Services.CouponAPI/Data/AppDbContext.cs
@@ -11,17 +11,25 @@ namespace Restaurant.Services.CouponAPI.Data
 
         public DbSet<Coupon> Coupons { get; set; }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-        //    modelBuilder.Entity<Coupon>().HasData(new Coupon
-        //    {
-        //        CouponId = 1,
-        //        CouponCode = "100FF",
-        //        DiscountAmount = 10,
-        //        MinAmount = 20
-        //    });
-        //}
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "100FF",
+                DiscountAmount = 10,
+                MinAmount = 20
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "200FF",
+                DiscountAmount = 20,
+                MinAmount = 40
+            });
+        }
     }
 }
--- Restaurant.Services.CouponAPI/Migrations/20250604135016_seedcoupontables.Designer.cs
+++ Restaurant.Services.CouponAPI/Migrations/20250604135016_seedcoupontables.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Restaurant.Services.CouponAPI.Data;
 
@@ -10,9 +11,11 @@ using Restaurant.Services.CouponAPI.Data;
 namespace Restaurant.Services.CouponAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250604135016_seedcoupontables")]
+    partial class seedcoupontables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
