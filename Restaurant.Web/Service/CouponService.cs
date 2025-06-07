4d43b3369c2fec440fc3817218d4464cc2d84712
maricel <maricelmb@gmail.com>
2025-06-07 13:06:19 +1000

    S3.30: Create ICouponService interface and do dependency injection

---
+++ Restaurant.Web/Service/CouponService.cs
@@ -0,0 +1,44 @@
+﻿using Restaurant.Web.Models;
+using Restaurant.Web.Service.IService;
+
+namespace Restaurant.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+        public CouponService(IBaseService baseService) 
+        {
+            _baseService = baseService;
+        }
+
+        Task<ResponseDto?> ICouponService.CreateCouponsAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ResponseDto?> ICouponService.DeleteCouponsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ResponseDto?> ICouponService.GetAllCouponAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ResponseDto?> ICouponService.GetCouponAsync(string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ResponseDto?> ICouponService.GetCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ResponseDto?> ICouponService.UpdateCouponsAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Restaurant.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,22 @@
+﻿using Restaurant.Services.CouponAPI.Model.Dto;
+using Restaurant.Web.Models;
+using CouponDto = Restaurant.Web.Models.CouponDto;
+using ResponseDto = Restaurant.Web.Models.ResponseDto;
+
+namespace Restaurant.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetCouponAsync(string couponCode);
+        Task<ResponseDto?> GetAllCouponAsync();
+
+        Task<ResponseDto?> GetCouponByIdAsync(int id);
+
+        Task<ResponseDto?> CreateCouponsAsync(CouponDto couponDto);
+
+        Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponDto);
+
+        Task<ResponseDto?> DeleteCouponsAsync(int id);
+
+    }
+}
