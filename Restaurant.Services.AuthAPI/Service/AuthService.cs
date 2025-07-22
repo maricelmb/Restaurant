dd81fab1ac6a8d2add736f205891e29f2303cadc
maricel <maricelmb@gmail.com>
2025-07-22 22:05:08 +1000

    Implement the login endpoint and service

--- Restaurant.Services.AuthAPI/Service/AuthService.cs
+++ Restaurant.Services.AuthAPI/Service/AuthService.cs
@@ -22,7 +22,29 @@ namespace Restaurant.Services.AuthAPI.Service
         }
         public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
-            throw new NotImplementedException();
+            var user = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.Username.ToLower());
+            bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+            if (user == null || isValid == false)
+            {
+                return new LoginResponseDto() { User = null, Token = "" };
+            }
+
+            UserDto userDto = new()
+            {
+                Email = user.Email,
+                ID = user.Id,
+                Name = user.Name,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            LoginResponseDto loginResponseDto = new LoginResponseDto()
+            {
+                User = userDto,
+                Token = ""
+            };
+
+            return loginResponseDto;
         }
 
         public async Task<string> Register(RegistrationRequestDto registrationRequestDto)
